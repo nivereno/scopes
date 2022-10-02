ce2138155c68713dea0849abcc45b880a79f3a48
nivereno@gmail.com <nivereno@gmail.com>
2022-10-02 07:50:27 +0300

    Initializing map from vector

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,13 @@ mod boot;
 mod timer;
 
 fn main() {
+    let SCOPES_SYMBOLS = vec![(String::from("TIMER_Main"), Symbol(KnownSymbol::TIMER_Main as u64)), (String::from(""), Symbol(KnownSymbol::SYM_Unnamed as u64)), (String::from("?corrupted?"), Symbol(KnownSymbol::SYM_Corrupted as u64)), (String::from("fn"), Symbol(KnownSymbol::KW_Fn as u64))];
     println!("{}", Symbol(42));
     let args = std::env::args();
-    let mut timers: HashMap<Symbol, Timer> = HashMap::new();
-    let mut symbols = BiMap::new();
-    symbols.insert(String::from("TIMER_Main"), Symbol(KnownSymbol::TIMER_Main as u64));
+    let mut timers = HashMap::new();
+    let mut symbols: BiMap<String, Symbol> = SCOPES_SYMBOLS.into_iter().collect();
+    //let mut symbols = BiMap::new();
+    //symbols.insert(String::from("TIMER_Main"), Symbol(KnownSymbol::TIMER_Main as u64));
     boot::sc_init(&mut timers);
     symbols.insert(String::from("Pone"), Symbol(42));
     symbols.insert(String::from("poner"), Symbol(0));
