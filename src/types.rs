527c62678cf974bfe83350eb4ea56f056e9221b2
nivereno@gmail.com <nivereno@gmail.com>
2023-01-04 14:12:21 +0300

    Implementing more type.rs functions

--- src/types.rs
+++ src/types.rs
@@ -58,31 +58,72 @@ impl Type {
         self.symbols.remove(&name);
     }
 
-    fn lookup_entry(&self, name: Symbol, dest: &TypeEntry) -> bool {
-        todo!()
+    fn lookup_entry(&self, name: &Symbol, dest: &mut TypeEntry) -> bool {
+        let mut T: Option<&Type> = Some(self);
+        while let Some(Type) = T {
+            if let Some(entry) = self.symbols.get(name) {
+                *dest = entry.clone();
+                return true;
+            }
+            //if Type == TYPE_Typename {
+            //    break;
+            //}
+            T = superof(Type);
+        }
+        return false;
     }
-    fn lookup_ref(&self, name: Symbol, dest: &ValueRef) -> bool {
-        todo!()
+    fn lookup_ref(&self, name: &Symbol, dest: &mut ValueRef) -> bool {
+        let mut entry = TypeEntry{expr: ValueRef {value: crate::valueref::Value::usize(0), anchor: crate::anchor::Anchor::from(Symbol(0), 0, 0, 0)}, doc: None};
+        if self.lookup_entry(name, &mut entry) {
+            *dest = entry.expr;
+            return true
+        }
+        return false
     }
-    fn lookup_local_entry(name: Symbol, dest: &TypeEntry) -> bool {
-        todo!()
+    fn lookup_local_entry(&self, name: &Symbol, dest: &mut TypeEntry) -> bool {
+        if let Some(entry) = self.symbols.get(name) {
+            *dest = entry.clone();
+            return true;
+        }
+        return false;
     }
-    fn lookup_local_ref(name: Symbol, dest: &ValueRef) -> bool {
-        todo!()
+    fn lookup_local_ref(&self, name: &Symbol, dest: &mut ValueRef) -> bool {
+        let mut entry = TypeEntry{expr: ValueRef {value: crate::valueref::Value::usize(0), anchor: crate::anchor::Anchor::from(Symbol(0), 0, 0, 0)}, doc: None};
+        if self.lookup_local_entry(name, &mut entry) {
+            *dest = entry.expr;
+            return true;
+        }
+        return false;
     }
-    fn lookup_call_handler(&self, dest: &ValueRef) -> bool {
-        return self.lookup_ref(Symbol(KnownSymbol::SYM_CallHandler as u64), dest);
+    fn lookup_call_handler(&self, dest: &mut ValueRef) -> bool {
+        return self.lookup_ref(&Symbol(KnownSymbol::SYM_CallHandler as u64), dest);
     }
-    fn lookup_return_handler(&self, dest: &ValueRef) -> bool {
-        return self.lookup_ref(Symbol(KnownSymbol::SYM_CallHandler as u64), dest);
+    fn lookup_return_handler(&self, dest: &mut ValueRef) -> bool {
+        return self.lookup_ref(&Symbol(KnownSymbol::SYM_CallHandler as u64), dest);
     }
-    fn lookup_quote_handler(&self, dest: &ValueRef) -> bool {
-        return self.lookup_ref(Symbol(KnownSymbol::SYM_CallHandler as u64), dest);
+    fn lookup_quote_handler(&self, dest: &mut ValueRef) -> bool {
+        return self.lookup_ref(&Symbol(KnownSymbol::SYM_CallHandler as u64), dest);
     }
 }
 
 fn is_opaque(T: &Type) -> bool {
-    todo!()
+    match T.kind() {
+        TypeKind::TK_Qualify => {todo!()},//return is_opaque(cast<QualifyType>(T)->type)},
+        TypeKind::TK_Typename => {
+            /*const TypenameType *tt = cast<TypenameType>(T);
+            if (tt->is_opaque()) {
+                return true;
+            } else {
+                // does this make sense?
+                return is_opaque(tt->storage());
+            }*/
+            todo!()
+        },
+            //case TK_Image: // can be loaded
+            //case TK_SampledImage: // can be loaded
+            TypeKind::TK_Function | TypeKind::TK_Arguments => {return true},
+            _ => {return false}
+        }
 }
 fn storage_kind(T: &Type) -> TypeKind {
     if is_opaque(T) {
@@ -97,16 +138,33 @@ fn bitsize_of(T: &Type) -> Result<usize, anyhow::Error> {
     todo!()
 }
 fn qualified_size_of(T: &Type) -> Result<usize, anyhow::Error> {
-    todo!()
+    return size_of(qualified_storage_type(T)?)
 }
 fn align_of(T: &Type) -> Result<usize, anyhow::Error> {
     todo!()
 }
 fn qualified_align_of(T: &Type) -> Result<usize, anyhow::Error> {
-    todo!()
+    return align_of(qualified_storage_type(T)?)
 }
-fn superof(T: &Type) -> &Type {
+fn superof(T: &Type) -> Option<&Type> {
     todo!()
+    /*match T.kind() {
+        TypeKind::TK_Qualify => {return TYPE_Qualify},
+        TypeKind::TK_Arguments => return TYPE_Arguments},
+        TypeKind::TK_Integer => {return TYPE_Integer},
+        TypeKind::TK_Real => {return TYPE_Real},
+        TypeKind::TK_Pointer => {return TYPE_Pointer},
+        TypeKind::TK_Array => {return (cast<ArrayType>(T)->is_zterm())?TYPE_ZArray:TYPE_Array},
+        TypeKind::TK_Vector => {return TYPE_Vector},
+        TypeKind::TK_Matrix => {return TYPE_Matrix},
+        TypeKind::TK_Tuple => {return TYPE_Tuple},
+        TypeKind::TK_Typename => {return cast<TypenameType>(T)->super()},
+        TypeKind::TK_Function => {return TYPE_Function},
+        TypeKind::TK_Image => {return TYPE_Image},
+        TypeKind::TK_SampledImage => {return TYPE_SampledImage},
+        TypeKind::TK_Sampler => {return TYPE_Immutable},
+    }
+    return Err(anyhow!("unhandled type kind; corrupt pointer?"))*/
 }
 fn is_returning(T: &Type) -> bool {
     //return T != TYPE_NoReturn
@@ -148,6 +206,9 @@ fn storage_type(T: &Type) -> Result<&Type, anyhow::Error> {
     todo!()
 }
 
+fn qualified_storage_type(T: &Type) -> Result<&Type, anyhow::Error> {
+    todo!()
+}
 //------------------------------------------------------------------------------
 // TYPE CHECK PREDICATES
 //------------------------------------------------------------------------------
