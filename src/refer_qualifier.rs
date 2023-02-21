136db0981b99730abae754e987651c5a10996b1e
nivereno@gmail.com <nivereno@gmail.com>
2023-02-21 14:35:54 +0300

    .

--- src/refer_qualifier.rs
+++ src/refer_qualifier.rs
@@ -1,7 +1,18 @@
-use crate::qualify_type::Qualifier;
+use crate::{qualify_type::Qualifier, symbol::Symbol, types::Type, pointer_type::pointer_type};
 
 
 struct ReferQualifier {
     this: Qualifier,
-    
+    //enum { Kind = QK_Refer };
+    flags: u64,
+    storage_class: Symbol
+}
+
+impl ReferQualifier {
+    pub fn new() {
+
+    }
+    pub fn get_pointer_type(&self, ET: &Type) -> &Type {
+        return pointer_type(ET, self.flags, self.storage_class.clone())
+    }
 }
